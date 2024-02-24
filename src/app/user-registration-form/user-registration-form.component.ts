bfdc1a5efd572dc9ccdafff39ca80ce5d8cd7078
Kirk Meyer <kirkmeyer03@gmail.com>
2024-02-24 12:20:16 -0800

    Created component to house user registration logic

---
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { FetchApiDataService } from '../fetch-api-data.service';
+
+@Component({
+  selector: 'app-user-registration-form',
+  templateUrl: './user-registration-form.component.html',
+  styleUrls: ['./user-registration-form.component.scss']
+})
+export class UserRegistrationFormComponent implements OnInit {
+
+  @Input() userData = { Username: '', Password: '', Email: '', Birthday: '' };
+
+  constructor(
+    public fetchApiData: FetchApiDataService,
+    public dialogRef: MatDialogRef<UserRegistrationFormComponent>,
+    public snackBar: MatSnackBar) { }
+  
+    ngOnInit(): void {
+    }
+
+  registerUser(): void {
+    this.fetchApiData.userRegistration(this.userData).subscribe((response) => {
+      console.log(response);
+      this.dialogRef.close();
+      this.snackBar.open('Account created successfully, log in to continue', 'OK', {
+        duration: 5000
+      });
+    }, (response) => {
+      console.log(response);
+      this.snackBar.open('Registration failed', 'OK', {
+        duration: 2000
+      });
+    });
+  }
+}
